0ff986447ee89253e7d0b3f1d837d51fbf47d7bf
xiao2wei <92316047+xiao2wei@users.noreply.github.com>
2022-12-25 11:44:51 +0800

    Add files via upload

---
+++ extra/data.0.js
@@ -0,0 +1,1 @@
+window["hzv3"]["pid"] = {"user":null,"locale":"zh-CN","preference":null,"projectAccessToken":"4FoF5AF9rnfgn3GUg14dU5","projectEncryptedPassword":"","isPasswordRequired":false,"is_on_premises":false,"is_wonder_share":false};
